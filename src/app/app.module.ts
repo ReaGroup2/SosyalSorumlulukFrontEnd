4f8be56ea1fa0de615181458719d651cb820a399
Görkem Irk <gorkemirk@gmail.com>
2023-09-15 13:43:16 +0300

    homepage e devam ediliyor slider çalışmıyor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './homepage/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavbarComponent,
+    FooterComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
